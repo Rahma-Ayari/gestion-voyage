04125200973d39674394d0055c6f0828582ec154
bennasrsafa <safabennasr14@gmail.com>
2026-02-01 12:39:21 +0100

    supprimer table role, visiteur, et mofification dans administrateur et personne et utilisateur

--- src/main/java/Entite/Personne.java
+++ src/main/java/Entite/Personne.java
@@ -1,27 +1,20 @@
 package Entite;
 
-public class Personne {
-    protected int idPersonne;
-    protected String nom;
-    protected String prenom;
+import java.util.Date;
+
+public class Personne extends Utilisateur {
+    private String nom;
+    private String prenom;
 
     public Personne() {
-
+        super();
     }
 
-    public Personne(int idPersonne, String nom, String prenom) {
-        this.idPersonne = idPersonne;
+    public Personne(int id, String email, String motDePasse, Date dateInscription, String nom, String prenom) {
+        super(id, email, motDePasse, dateInscription);
         this.nom = nom;
         this.prenom = prenom;
     }
-
-    public Personne(String nom, String prenom) {
-        this.nom = nom;
-        this.prenom = prenom;
-    }
-
-    public int getIdPersonne() { return idPersonne; }
-    public void setIdPersonne(int idPersonne) { this.idPersonne = idPersonne; }
 
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
@@ -31,6 +24,6 @@ public class Personne {
 
     @Override
     public String toString() {
-        return "ID: " + idPersonne + " | Nom: " + nom + " | Prenom: " + prenom;
+        return "Personne [ID=" + idUtilisateur + ", Nom=" + nom + ", Prenom=" + prenom + ", Email=" + email + "]";
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(MysqlxDatatypes.Scalar.String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
