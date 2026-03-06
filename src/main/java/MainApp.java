6eb9b73e7c1e0111506e70eee8f2b61104abbb01
dorsafmadani <madanidorsaf@gmail.com>
2026-03-06 09:14:45 +0100

    Refactor start method and improve error handling
    Refactored start method to use primaryStage and added error handling for FXML loading.

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -5,22 +5,44 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import Service.ServiceVol;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class MainApp extends Application {
 
- @Override
-public void start(Stage stage) throws Exception {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Chargement du fichier FXML
+            // Note : Adaptez le chemin si votre FXML est dans un sous-dossier (ex: "/View/Destination.fxml")
+             ServiceVol serviceVol = new ServiceVol();
 
-    Parent root = FXMLLoader.load(
-            getClass().getResource("/VolView.fxml")
-    );
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/VolView.fxml"));
+            Parent root = loader.load();
 
-    Scene scene = new Scene(root);
+            // Création de la scène
+            Scene scene = new Scene(root);
+            primaryStage.setMaximized(true); // plein écran
+            // Configuration du Stage (Fenêtre)
+            primaryStage.setTitle("TripEase - Gestion des Destinations");
 
-    stage.setScene(scene);
-    stage.show();
-}
+            // Ajout d'une icône à la fenêtre (optionnel)
+            // primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/tripease_logo2.png")));
 
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            System.err.println("Erreur lors du chargement du FXML : " + e.getMessage());
+            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
